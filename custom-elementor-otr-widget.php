9728682105094c29181c9a0094682f8ad74847ed
eagle4life69 <contact@eagle4life69.com>
2025-06-24 17:09:47 -0700

    Update custom-elementor-otr-widget.php

--- custom-elementor-otr-widget.php
+++ custom-elementor-otr-widget.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Custom Elementor OTR Widget
 Description: Lists OTR episodes with tabs by year and download links.
-Version: 2.6.3.5
+Version: 2.6.3.6
 Author: Andrew Rhynes
 GitHub Plugin URI: https://github.com/eagle4life69/custom-elementor-otr-widget
 GitHub Branch: main
